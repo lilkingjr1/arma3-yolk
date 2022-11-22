ac479d0a115af42b560a3c993562177f3be3055b
Red-Thirten <rehlmgaming@gmail.com>
2022-11-22 15:13:18 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,12 @@ sleep 1
 export HOME=/home/container
 cd /home/container || exit 1
 
+# Download and install SteamCMD
+mkdir -p tmp
+cd /tmp
+curl -sSL -o steamcmd.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+tar -xzvf steamcmd.tar.gz -C /home/container/steamcmd
+cd /home/container
 
 ./steamcmd/steamcmd.sh +force_install_dir /mnt/server "+login \"${STEAM_USER}\" \"${STEAM_PASS}\"" +app_update ${STEAMCMD_APPID} validate +quit
 echo -e "\nUPDATE CHECK COMPLETE!\n"
