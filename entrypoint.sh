b08f124655af148d9a9677a2f27af54966ad0c32
Red-Thirten <rehlmgaming@gmail.com>
2022-11-22 14:13:09 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,17 +26,18 @@
 sleep 1
 
 # Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
+# TZ=${TZ:-UTC}
+# export TZ
 
 # Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
+# INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+# export INTERNAL_IP
 
 # Switch to the container's working directory
+export HOME=/home/container
 cd /home/container || exit 1
 
 
-./steamcmd/steamcmd_old.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} +app_update ${STEAMCMD_APPID} $( [[ -z ${VALIDATE_SERVER} ]] || printf %s "validate" ) +quit
+./steamcmd.sh +force_install_dir /mnt/server "+login \"${STEAM_USER}\" \"${STEAM_PASS}\"" +app_update ${STEAMCMD_APPID} validate +quit
 echo -e "\nUPDATE CHECK COMPLETE!\n"
 exit 0
